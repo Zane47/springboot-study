01177f981960bca75ee3d655429787c1d6958e8e
Zane47 <1228073191@qq.com>
2022-03-20 15:04:39 +0800

    fix: change

--- foodie-study/foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
+++ foodie-study/foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -1,10 +1,8 @@
-package com.imooc.controller.shop;
+package com.imooc.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletRequest;
